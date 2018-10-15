420cc78dd6de86ff873406527e56eb26473f496c
RSheme <rami.shemeikka@gmail.com>
2018-10-15 18:13:33 +0300

    Completely rewritten.

--- loop-tasks/task9/task9/Program.cs
+++ loop-tasks/task9/task9/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace task9
 {
@@ -7,34 +9,37 @@ namespace task9
         static void Main(string[] args)
         {
             Random coin = new Random();
-            int kruuna = 0;
+            int[] result = new int[2];
+            List<string> breaklist = new List<string>() { "quit", "exit", "stop", "leave", "close", "break", "escape", "disintegrate", "i am bored", "divide by zero", "termin", "lopet", "loppu", "poistu", "sulje", "ovi auki tai tapahtuu hirveitä" };
             int count = 1;
-            string input;
-            bool inputcheck = true;
+            string input = "";
 
-            do
+            while (true)
             {
                 Console.Clear();
                 Console.Write("Ohjelma heittää kolikkoa käyttäjän antaman määrän ja kertoo montako kertaa tuli kruuna tai klaava.\n\n");
 
-                if (count != 1)
+                if (count < 1)
                     Console.Write("Virheellinen syöte.\n");
+                else if (input == count.ToString())
+                {
+                    Console.Write($"Kruuna heitettiin {result[0]} kertaa ja klaava {result[1]} kertaa.\n");
+                    Array.Clear(result, 0, result.Length);
+                }
 
                 Console.Write("Syötä numero: ");
-
                 input = Console.ReadLine();
-                inputcheck = Int32.TryParse(input, out count);
 
-            } while (!inputcheck || count < 1);
+                if (breaklist.Any(input.ToLower().Contains))
+                    break;
 
-            for (int i = 1; i <= count; i++)
-            {
-                if (coin.Next(2) == 0)
-                    kruuna++;
+                Int32.TryParse(input, out count);
+
+                for (int i = 1; count > 0 && i <= count; i++)
+                {
+                    result[coin.Next(2)]++;
+                }
             }
-
-            Console.Clear();
-            Console.Write($"Ohjelma heittää kolikkoa käyttäjän antaman määrän ja kertoo montako kertaa tuli kruuna tai klaava.\n\nKruuna heitettiin {kruuna} kertaa ja klaava {count - kruuna} kertaa.\n");
         }
     }
 }
